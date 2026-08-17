1f72618480279e13958399462a73e0394b1f8ddb
XieXiangyu <1837563697@qq.com>
2026-08-17 10:58:40 +0800

    迁移: 用户游戏设置增加音符飞行速度字段(real, 默认1.0=原速回填存量记录)

---
+++ Fantasy.Packages/Fantasy.Net/Migrations/20260817023718_用户游戏设置增加音符飞行速度.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fantasy.Net.Migrations
+{
+    /// <inheritdoc />
+    public partial class 用户游戏设置增加音符飞行速度 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "音符飞行速度",
+                schema: "NetShare_音游",
+                table: "用户游戏设置",
+                type: "real",
+                nullable: false,
+                defaultValue: 1f); // 存量记录按原速(1.0x)回填
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "音符飞行速度",
+                schema: "NetShare_音游",
+                table: "用户游戏设置");
+        }
+    }
+}
